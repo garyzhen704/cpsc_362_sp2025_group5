cecfba7bc6c3d7ac98705771d2d4acb2b7cb64de
garyzhen704 <gzhen@csu.fullerton.edu>
2025-03-02 08:44:38 -0800

    added classes for player and dealer

--- blackjack.py
+++ blackjack.py
@@ -5,3 +5,17 @@ class Card:
 
 card = Card("Ace")
 print(card.value)
+
+
+#Player action and Dealer - Gary
+class Player:
+    def __init__(self, game, balance=100):
+        self.game = game
+        self.hand = []
+        self.balance = balance
+        self.bet = 0
+
+class Dealer:
+    def __init__(self, game):
+        self.game = game
+        self.hand = []
