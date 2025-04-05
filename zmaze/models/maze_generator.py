1637268fdd3227a651b569513604088d623bbd06
garyzhen704 <gzhen@csu.fullerton.edu>
2025-04-05 01:42:56 -0700

    added passage func implementation

--- zmaze/models/maze_generator.py
+++ zmaze/models/maze_generator.py
@@ -20,3 +20,18 @@ class MazeGenerator:
         self.maze[self.height-1][self.width-2] = 0
 
         return self.maze
+    
+    def _carve_passages(self, x, y):
+        #directions - up,down,right,left
+        directions = [(2, 0), (0, 2) (-2, 0), (0, -2)]
+        random.shuffle(directions)
+
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+
+            #check if new cell is within bounds
+            if 0 < nx < self.width-1 and 0 < ny < self.height-1 and self.maze[ny][nx] == 1:
+                self.maze[y+dy//2][x+dx//2] = 0 #creates path betweel cells
+                self.maze[ny][nx] = 0
+
+                self._carve_passages(nx, ny)
