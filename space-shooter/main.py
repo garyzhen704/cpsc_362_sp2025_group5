aa7146e1c0c69c4db2ded9eb30fab6a61fd96458
donodj <donovan.jab0613@gmail.com>
2025-04-14 01:26:25 -0700

    add acceleration and deceleration to player

--- space-shooter/main.py
+++ space-shooter/main.py
@@ -21,13 +21,16 @@ WHITE = (255, 255, 255)
 
 # Player properties
 player_size = 15
-player_pos = Vector(screen_width // 2 - player_size // 2, screen_height // 2 - player_size // 2)
-player_speed = 360  # in pixels per second
+player_pos = Vector(screen_width / 2, screen_height / 2)  # Start at screen center
+player_vel = Vector(0, 0)
+player_max_speed = 360  # Pixels per sec
+player_accel = 15  # Pixels per sec ^ 2
+player_decel = 0.5
 
 
 def get_input_dir() -> Vector:
     dir = Vector(0, 0)
-    
+
     keys = pygame.key.get_pressed()
     if keys[K_LEFT] or keys[K_a]:
         dir.x -= 1.0
@@ -37,8 +40,9 @@ def get_input_dir() -> Vector:
         dir.y -= 1.0
     if keys[K_DOWN] or keys[K_s]:
         dir.y += 1.0
-    
+
     return dir.normalized()
+
 
 def clamped_to_screen(vec: Vector) -> Vector:
     return Vector(vec.x % screen_width, vec.y % screen_height)
@@ -51,12 +55,18 @@ while True:
             pygame.quit()
             sys.exit()
 
-    player_pos += get_input_dir() * (player_speed / FPS)
+    input_dir = get_input_dir()
+    throttle_on = input_dir.magnitude() > 0.1
+
+    target_vel = input_dir * (player_max_speed / FPS)
+    player_vel = player_vel.moved_toward(target_vel, (player_accel if throttle_on else player_decel) / FPS)
+    
+    player_pos += player_vel
     player_pos = clamped_to_screen(player_pos)
 
     # Draw shapes
     screen.fill(BLACK)
     pygame.draw.circle(screen, WHITE, tuple(player_pos), player_size)
-
     pygame.display.update()
+
     fps_clock.tick(FPS)
--- space-shooter/vector.py
+++ space-shooter/vector.py
@@ -1,35 +1,42 @@
 class Vector():
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+    # Returns length of vector
     def magnitude(self) -> float:
         return (self.x * self.x + self.y * self.y) ** 0.5
-    
+
+    # Returns a copy of this vector with a length of 1
     def normalized(self) -> 'Vector':
-        if (self.magnitude() > 1):
-            pass
         return self / self.magnitude()
+
+    # Returns a copy of this vector moved towards another by a specified number of units
+    def moved_toward(self, to: 'Vector', amount: float) -> 'Vector':
+        dir = to - self
+        if amount >= dir.magnitude():
+            return to
+        return self + (dir.normalized() * amount)
 
     def __add__(self, other: 'Vector') -> 'Vector':
         return Vector(self.x + other.x, self.y + other.y)
-    
+
     def __sub__(self, other: 'Vector') -> 'Vector':
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, scalar) -> 'Vector':
         return Vector(self.x * scalar, self.y * scalar)
-    
+
     def __truediv__(self, scalar) -> 'Vector':
         if scalar == 0:
             return Vector(0, 0)
         return Vector(self.x / scalar, self.y / scalar)
-    
+
     def __floordiv__(self, scalar) -> 'Vector':
         if scalar == 0:
             return Vector(0, 0)
         return Vector(self.x // scalar, self.y // scalar)
-    
+
     def __iter__(self):
         return iter((self.x, self.y))
 
